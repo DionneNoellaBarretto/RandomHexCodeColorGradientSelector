e8eac4a50d14df12da78d091de8615db202c4eed
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-08-14 19:44:25 -0400

    updated code to include a plain background and a gradient shade using 3 colors

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RandomHexCodeColorGradientSelector
+
+######As someone who loves to experiment with various combinations of colors I've created a easy bookmark'able project that allows frontend enthusiasts looking to experiment with background colors and gradient shades to click until they find a color scheme that suits their taste. 
+
+📄 [Github Page](https://dionnenoellabarretto.github.io/RandomHexCodeColorGradientSelector/)
+🗂️ [Github Repository](https://github.com/DionneNoellaBarretto/RandomHexCodeColorGradientSelector)
+
+This application is built using HTML, CSS, JavaScript & Bootstrap.
+
+**Example Screenshot:**
+<img src="./ApplicationExample.png">
+
+
+
---
+++ script.js
@@ -0,0 +1,35 @@
+var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+
+function changePlainColor()
+{
+
+  var hexcode = '';
+  
+  for(var i = 0; i<6; i++){
+    var random_index = Math.floor(Math.random() * hex_numbers.length);
+    hexcode += hex_numbers[random_index];
+  }
+// https://www.w3schools.com/jsref/prop_style_background.asp
+  document.getElementById("hex-code1").innerHTML = hexcode;
+  document.getElementById("plaincontainer").style.backgroundColor = '#' + hexcode;
+}
+document.querySelector("#submit").addEventListener("click", () => {
+    var hexcodea = "";
+    var hexcodeb = "";
+    var hexcodec = "";
+    var random_index = 0;
+    
+    for(let i = 0; i < 6;i++){
+      random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcodea += hex_numbers[random_index];
+      random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcodeb += hex_numbers[random_index];
+      random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcodec += hex_numbers[random_index];
+    }
+// https://forum.freecodecamp.org/t/gradual-css-linear-gradient/195068
+    document.getElementById("mixcontainer").style.background = `linear-gradient(-45deg, #${hexcodea}, #${hexcodeb},#${hexcodec})`;
+    document.querySelector("#hexcodea").textContent = hexcodea;
+    document.querySelector("#hexcodeb").textContent = hexcodeb;
+    document.querySelector("#hexcodec").textContent = hexcodec;
+  });
